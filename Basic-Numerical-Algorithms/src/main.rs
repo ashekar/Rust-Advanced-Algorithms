3f55e6399197cdf626740cee4c7137e38b9ebcd2
Aditya Shekar <ashekar1@alumni.cmu.edu>
2022-02-18 22:08:46 -0700

    Fibonacci and Power of Two

--- Basic-Numerical-Algorithms/src/main.rs
+++ Basic-Numerical-Algorithms/src/main.rs
@@ -5,17 +5,57 @@ use std::env::{args, Args};
 fn main() {
     let mut args: Args = args();
     let first = args.nth(1).unwrap();
+    let first_number: i32= first.parse::<i32>().unwrap();
+    /*
     let operator = args.nth(0).unwrap().chars().next().unwrap();
     let second = args.nth(0).unwrap();
 
-    let first_number: f32= first.parse::<f32>().unwrap();
+
     let second_number: f32 = second.parse::<f32>().unwrap();
 
-    let result = operate(operator, first_number, second_number);
 
-    println!("{:?}", output(first_number, operator, second_number, result));
+    let result = operate(operator, first_number, second_number);
+    */
+
+    let result: i32 = nthFibonacciNumber(first_number);
+
+    println!("{:?}", result);
 
 } 
+
+fn nthFibonacciNumber(index: i32) -> i32{
+   
+    let mut a: i32 = 1;
+    let mut b: i32 = 1;
+    let mut temp:i32 = 0;
+    let mut i: i32 = 1;
+
+    while i < index{
+        temp = b;
+        b = a + b;
+        a = temp;
+        i = i + 1
+    }
+    return b;
+}
+
+fn powerOfTwo(number_to_consider: i32) -> bool{
+    let mut i: i32 = 0;
+    let mut numberOfOnes: i32 = 0;
+    let mut movingValue = number_to_consider;
+    let mask: i32 = 0x1;
+    while i < 32{
+        if movingValue & mask == mask{
+            numberOfOnes = numberOfOnes + 1;
+        }
+        movingValue = movingValue >> mask;
+        i = i + 1;
+    }
+    if numberOfOnes > 1i32{
+        return false;
+    }
+    return true;
+}
 
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32{
     if operator == '+' {
